888d0eb0abf574251dc3fa0e838ab6328626f80f
yangduojun <319202901@qq.com>
2022-04-15 15:42:04 +0800

    项目初始结构

---
+++ gulimall-product/src/test/java/com/ydj/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydj.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
